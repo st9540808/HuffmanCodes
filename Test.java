288562e6d2ed4c2a47df039ae173b73845e70bf1
st9540808 <st9540808@gmail.com>
2017-05-28 23:52:36 +0800

    add try catch clause to Test example

--- Test.java
+++ Test.java
@@ -1,11 +1,17 @@
 import Huffman.*;
 
 public class Test {
-	public static void main(String[] args) throws InvalidFormatException {
+	public static void main(String[] args) {
 		String compressedStr = Huffman.compress("this is a test string!!!");
-		String decompressedStr = Huffman.decompress(compressedStr);
-
 		System.out.println(compressedStr);
-		System.out.println(decompressedStr);
+
+		String decompressedStr;
+		try {
+			decompressedStr = Huffman.decompress(compressedStr);
+			System.out.println(decompressedStr);
+		}
+		catch (InvalidFormatException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
