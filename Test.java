775f57b2c7025e60ab173b9b95109a194a83f7cf
st9540808 <st9540808@gmail.com>
2017-05-28 23:38:40 +0800

    add test example

--- Test.java
+++ Test.java
@@ -1,7 +1,11 @@
 import Huffman.*;
 
 public class Test {
-	public static void main(String[] args) {
-		System.out.println(Huffman.compress("eeeeeeeeeeedddddddddddd"));
+	public static void main(String[] args) throws InvalidFormatException {
+		String compressedStr = Huffman.compress("this is a test string!!!");
+		String decompressedStr = Huffman.decompress(compressedStr);
+
+		System.out.println(compressedStr);
+		System.out.println(decompressedStr);
 	}
 }
