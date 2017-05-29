23112dbbd35c8b36df41db432b455e1b9eddfb2d
st9540808 <st9540808@gmail.com>
2017-05-29 12:41:23 +0800

    update Test class and add comment

--- Test.java
+++ Test.java
@@ -2,16 +2,23 @@ import Huffman.*;
 
 public class Test {
 	public static void main(String[] args) {
+		// to compress a string
 		String compressedStr = Huffman.compress(args[0]);
-		System.out.println(compressedStr);
+		System.out.println("str after compressed : " + compressedStr);
 
+		// to decompress a compressed string
 		String decompressedStr;
 		try {
 			decompressedStr = Huffman.decompress(compressedStr);
-			System.out.println(decompressedStr);
+			System.out.println("after decompressed   : " + decompressedStr);
 		}
 		catch (InvalidFormatException e) {
 			System.out.println(e.getMessage());
 		}
+
+		// check size
+		System.out.println("");
+		System.out.println("original str length  : " + args[0].length());
+		System.out.println("after compressed     : " + compressedStr.length());
 	}
 }
